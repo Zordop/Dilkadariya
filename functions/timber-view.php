62cc281fd6ec2ec3a3eff7d73c66a0a20bd9aa03
Jared Novack <jarednova@upstatement.com>
2014-06-03 19:58:38 -0400

    wrote some scaffold for timber view

---
+++ functions/timber-view.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+
+$view = new TimberView();
+$view->context['post'] = new TimberPost();
+$view->cache(600);
+echo $timber->render('single.twig');
+
+
+*/
+
+class TimberView {
+	
+	public function __construct(){
+
+	}
+
+	public function compile(){
+
+	}
+
+	public function render(){
+
+	}
+
+}
